4b572a30ce06610e9ce62607efce77e49938714a
Ryan Garrity <rgarrity7000@gmail.com>
2020-08-05 21:01:17 -0400

    Initted everything

--- Game/Effects/LimboEffects.cs
+++ Game/Effects/LimboEffects.cs
@@ -13,9 +13,13 @@ namespace RewindGame.Game.Effects
     {
         public ContentManager Content;
         public RewindGame parentGame;
-        private Texture2D texture1;
-        private Texture2D texture3;
-        private Texture2D texture2;
+        private Texture2D limboStatic;
+        private Texture2D limboHorizontal0;
+        private Texture2D limboHorizontal1;
+        private Texture2D limboHorizontal2;
+        private Texture2D limboVertical0;
+        private Texture2D limboVertical1;
+        private Texture2D limboVertical2;
         public LimboEffects(RewindGame parent_game, ContentManager content) 
         { 
             Content = content;
@@ -24,9 +28,13 @@ namespace RewindGame.Game.Effects
             // see GameObject
 
             //texturename = Content.Load<Texture2D>( texturePath );
-            texture1 = Content.Load<Texture2D>("effects/backgrounds/limbo/limbohorizontal0");
-            texture2 = Content.Load<Texture2D>("effects/backgrounds/limbo/limbohorizontal1");
-            texture3 = Content.Load<Texture2D>("effects/backgrounds/limbo/limbohorizontal2");
+            limboStatic = Content.Load<Texture2D>("effects/backgrounds/limbo/limbostatic");
+            limboHorizontal0 = Content.Load<Texture2D>("effects/backgrounds/limbo/limbohorizontal0");
+            limboHorizontal1 = Content.Load<Texture2D>("effects/backgrounds/limbo/limbohorizontal1");
+            limboHorizontal2 = Content.Load<Texture2D>("effects/backgrounds/limbo/limbohorizontal2");
+            limboVertical0 = Content.Load<Texture2D>("effects/backgrounds/limbo/limbovertical0");
+            limboVertical1 = Content.Load<Texture2D>("effects/backgrounds/limbo/limbovertical1");
+            limboVertical2 = Content.Load<Texture2D>("effects/backgrounds/limbo/limbovertical2");
         }
 
         public void Update(StateData state)
@@ -38,16 +46,17 @@ namespace RewindGame.Game.Effects
 
         public void DrawBackground(StateData state, SpriteBatch sprite_batch)
         {
-            Vector2 CameraPosReal = state.camera_position - new Vector2(parentGame.activeLevel.screensHorizontal, parentGame.activeLevel.screensVertical);
+            Vector2 CameraPosReal = state.camera_position - new Vector2(RewindGame.LEVEL_SIZE_X/2, RewindGame.LEVEL_SIZE_Y/2);
             // remember to use state.levelcenter and state.camera_offset
             // state.levelcenter- the center of the current level, where you should be drawing the background
             // state.camera_position- the offset of the camera from the levelcenter (for levels larger than one screen)
             // background
             //sprite_batch.Begin(SpriteSortMode.Deferred, null, SamplerState.LinearWrap, null, null);
-            sprite_batch.Draw(texture1, CameraPosReal,  Color.White);
-            sprite_batch.Draw(texture2, CameraPosReal,  Color.White);
-            sprite_batch.Draw(texture3, CameraPosReal,  Color.White);
-            
+            //sprite_batch.Draw(limboStatic, CameraPosReal, Color.White);
+            sprite_batch.Draw(limboHorizontal0, CameraPosReal,  Color.White);
+            sprite_batch.Draw(limboHorizontal1, CameraPosReal,  Color.White);
+            sprite_batch.Draw(limboHorizontal2, CameraPosReal,  Color.White);
+
 
         }
 
