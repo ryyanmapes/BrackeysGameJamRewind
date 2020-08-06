219025ca808016b8ee79fdfa2bc4b4e93d3815c1
Ryan Garrity <rgarrity7000@gmail.com>
2020-08-05 23:53:46 -0400

    wrote some notes

--- Game/Effects/OverlayEffects.cs
+++ Game/Effects/OverlayEffects.cs
@@ -28,7 +28,12 @@ namespace RewindGame.Game.Effects
 
         public void TriggerDeath()
         {
-
+            //when death runstate is runstate.platerdead get with parentgame.runstate
+            //parentgame.statetimer tells how long will stay in state
+            //rewindgame.playerdeathtime is max death time (ticks down to 0(when player respawns))
+            //
+            //render a big black squear on entire screen (content/debug) if turned to white can be any color with draw function
+            //fade into black starting at about halfway through platyerdeath time (when playerdeath time hits 50% fade to black) fade out to black when it is no longer runstate.playerdead
         }
 
         public void Update(StateData state)
