28820afcc481d36b7fb592064651ff5a3e90cdbb
JakasaurusRex <48461874+JakasaurusRex@users.noreply.github.com>
2020-08-06 19:17:20 -0600

    Sound stuff

--- Game/Sound/SoundManager.cs
+++ Game/Sound/SoundManager.cs
@@ -20,6 +20,7 @@ namespace RewindGame.Game.Sound
         public ChaiFoxes.FMODAudio.Sound playerJumpSound;
         public ChaiFoxes.FMODAudio.Sound playerLandSound;
         public ChaiFoxes.FMODAudio.Sound playerDieSound;
+        public ChaiFoxes.FMODAudio.Sound peltingRain;
         public float fadeIntoLoop2;
         public bool pianoFadeoutInc = false;
         public float fadeIntoPiano;
@@ -49,8 +50,9 @@ namespace RewindGame.Game.Sound
 
             this.loop1 = StudioSystem.GetEvent("Event:/Music/Limbo/Loop1").CreateInstance();
             this.loop2 = StudioSystem.GetEvent("Event:/Music/Limbo/Loop2").CreateInstance();
-
-
+            this.peltingRain = CoreSystem.LoadStreamedSound("peltingrain.wav");
+            peltingRain.Volume = 0;
+            peltingRain.Play();
 
 
 
@@ -122,10 +124,20 @@ namespace RewindGame.Game.Sound
         }
 
         // 0 = light rain, 1 = bad storm
-        public void ModifyOverrain(float intensity) { }
-        public void EndOverrain() { }
+        public void ModifyOverrain(float intensity) { 
+            if(intensity > 0)
+            {
+                peltingRain.Volume = intensity;
+            }
+        }
+        public void EndOverrain() {
+            peltingRain.Volume = 0;
+        }
 
-        public void TriggerLightining() { }
+        public void TriggerLightining() {
+            SoundEffect lightningEffect = Content.Load<SoundEffect>("sfx/thunderstrike");
+            lightningEffect.Play();
+        }
 
         public void TriggerPlayerWalljump()
         {
