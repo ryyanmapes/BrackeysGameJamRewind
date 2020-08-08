635588b32748273a48dc71f7314a26cb6d456b3e
ryyanmapes <ryyanmapes@gmail.com>
2020-08-08 03:05:23 -0600

    fixes

--- Game.cs
+++ Game.cs
@@ -513,7 +513,7 @@ namespace RewindGame
             else if (Keyboard.GetState().IsKeyDown(Keys.J))
                 loadLevelAndConnections("limbofinal");
             else if (Keyboard.GetState().IsKeyDown(Keys.P))
-                loadLevelAndConnections("limbo18");
+                loadLevelAndConnections("cottonfinal");
             else if (Keyboard.GetState().IsKeyDown(Keys.U)) LoadArea(AreaState.cotton);
             else if (Keyboard.GetState().IsKeyDown(Keys.N)) LoadArea(AreaState.eternal);
             else if (Keyboard.GetState().IsKeyDown(Keys.NumPad8))
