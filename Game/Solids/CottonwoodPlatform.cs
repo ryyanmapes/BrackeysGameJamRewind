5eb85efe5892e0f808fc938ae75de7ca075bcf75
Ryan Garrity <rgarrity7000@gmail.com>
2020-08-22 17:19:41 -0400

    Fixed an issue with cotton platform and eternal platform (eternal 2 still crashes but workin on it)
    Signed-off-by: Ryan Garrity <rgarrity7000@gmail.com>

--- Game/Solids/CottonwoodPlatform.cs
+++ Game/Solids/CottonwoodPlatform.cs
@@ -8,20 +8,37 @@ using RewindGame.Game.Graphics;
 namespace RewindGame.Game.Solids
 {
 
-    class CottonwoodPlatform : LimboPlatform
+    class CottonwoodPlatform : Platform
     {
 
-        protected new AnimationPlayer renderLongDown = new AnimationPlayer("cottonwood/cottonwoodplatformdownlong", 1, 1, true, 1, Vector2.Zero);
-        protected new AnimationPlayer renderLongUp = new AnimationPlayer("cottonwood/cottonwoodplatformdownlong", 1, 1, true, 1, Vector2.Zero);
-        protected new AnimationPlayer renderDown = new AnimationPlayer("cottonwood/cottonwoodplatformdown", 1, 1, true, 1, Vector2.Zero);
-        protected new AnimationPlayer renderUp = new AnimationPlayer("cottonwood/cottonwoodplatformup", 1, 1, true, 1, Vector2.Zero);
+        protected  AnimationPlayer renderLongDown = new AnimationPlayer("cottonwood/cottonwoodplatformdownlong", 1, 1, true, 1, Vector2.Zero);
+        protected  AnimationPlayer renderLongUp = new AnimationPlayer("cottonwood/cottonwoodplatformdownlong", 1, 1, true, 1, Vector2.Zero);
+        protected  AnimationPlayer renderDown = new AnimationPlayer("cottonwood/cottonwoodplatformdown", 1, 1, true, 1, Vector2.Zero);
+        protected  AnimationPlayer renderUp = new AnimationPlayer("cottonwood/cottonwoodplatformup", 1, 1, true, 1, Vector2.Zero);
 
-        public new static CottonwoodPlatform Make(Level level, Vector2 starting_pos, Vector2 velocity_, bool isLong)
+        public  static CottonwoodPlatform Make(Level level, Vector2 starting_pos, Vector2 velocity_, bool isLong)
         {
             var tile = new CottonwoodPlatform();
             tile.Initialize(level, starting_pos, velocity_, isLong);
             return tile;
         }
+        public virtual void Initialize(Level level, Vector2 starting_pos, Vector2 velocity_, bool is_long)
+        {
 
+            // I know this looks like an awful mess, but trust me it's better than the ten lines I had before
+            renderer = is_long ? ((velocity_.Y < 0) ? renderLongDown : renderLongUp) : ((velocity_.Y < 0) ? renderDown : renderUp);
+
+            base.Initialize(level, starting_pos, velocity_, is_long ? 4 : 2);
+        }
+
+        public override void Draw(StateData state, SpriteBatch sprite_batch)
+        {
+            if (velocity.X < 0) spriteEffects = SpriteEffects.FlipHorizontally;
+            else spriteEffects = SpriteEffects.None;
+
+            base.Draw(state, sprite_batch);
+
+            ((AnimationPlayer)renderer).UpdateAnimation(state, state.getTimeN());
+        }
     }
 }
